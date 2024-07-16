86562905650918d91564e0242dcb969830e9dc8e
Antsistra <hadinurmuhamad8@gmail.com>
2024-07-16 18:35:50 +0700

    chore: Initialize project structure and dependencies

---
+++ src/components/pages/Landing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl font-bold">Hello world</h1>
+    </div>
+  );
+}
